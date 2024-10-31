0fef932e7a89b8873a9292745c3944d7ce769909
thiagokremer <thiagokremer@hotmail.com>
2024-10-30 22:53:17 -0300

    feat: estruturação e estilização do header e sessão de home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'mobile':'400px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+
+    extend: {
+      backgroundImage:{
+        'gradiente': 'linear-gradient(309deg, rgba(24,30,65,1) 70%, rgba(152,77,56,1) 100%)',
+      },
+
+      colors: {
+        'principal': '#f4f4f4',
+        'secundario': '#f4f4f480',
+      },
+
+      fontSize: {
+        'titulo': '70px',
+        'subtitulo': '50px',
+        'paragrafo': '20px',
+      }
+
+    },
   },
   plugins: [],
 }
